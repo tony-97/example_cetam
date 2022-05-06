6574743540e9d1256440535b92462c1433326ab7
Tony Angello <taaffaat_12@hotmail.com>
2022-05-05 19:44:59 -0500

    random numbers generated

--- buble_sort.c
+++ buble_sort.c
@@ -1,3 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+#define SIZE 10
+
+int main(void)
+{
+    int vec[SIZE] = { 0 };
+    srand(time(NULL));
+
+    for (size_t i = 0; i < SIZE; ++i) {
+        vec[i] = rand() % 100;
+    }
+
+    printf("[");
+    for (size_t i = 0; i < SIZE; ++i) {
+        printf("%d%s", vec[i], i == SIZE - 1 ? "" : ",");
+    }
+    printf("]\n");
+    return 0;
+}
