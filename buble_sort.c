d7d299e55ebc49a31b16ddb4032385b42a7f1307
Tony Angello <taaffaat_12@hotmail.com>
2022-05-05 19:57:27 -0500

    buble sort algorithm

--- buble_sort.c
+++ buble_sort.c
@@ -13,10 +13,22 @@ int main(void)
         vec[i] = rand() % 100;
     }
 
+    printf("===Random Vector===\n");
     printf("[");
     for (size_t i = 0; i < SIZE; ++i) {
-        printf("%d%s", vec[i], i == SIZE - 1 ? "" : ",");
+        printf("%d%s", vec[i], i == SIZE - 1 ? "" : ", ");
     }
     printf("]\n");
+
+    for (size_t i = 0; i < SIZE; ++i) {
+        for (size_t j = i; j < SIZE; ++j) {
+            if (vec[i] > vec[j]) {
+                int tmp = vec[i];
+                vec[i] = vec[j];
+                vec[j] = tmp;
+            }
+        }
+    }
+
     return 0;
 }
